3a8ab72626f3d84d2abe7bf5bef14c1966b20583
isobe-h <isobe-h@toack.co.jp>
2023-03-15 12:36:21 +0900

    update eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,29 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [],
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["react", "react-hooks", "@typescript-eslint"],
-  rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/function-component-definition": [
-      2,
-      { namedComponents: "arrow-function" },
-    ],
-  },
-};
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'airbnb',
+		'airbnb-typescript',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parserOptions: {
+		project: './tsconfig.json',
+	},
+	plugins: ['react', 'react-hooks', '@typescript-eslint'],
+	rules: {
+		'react-hooks/rules-of-hooks': 'error',
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/function-component-definition': [
+			2,
+			{ namedComponents: 'arrow-function' },
+		],
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: true, 
+				optionalDependencies: false,
+			},
+		],
+	},
+}
